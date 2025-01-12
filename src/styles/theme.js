3c0ef1e691abf00732f03882fd056806f4e2f530
daviddiema7 <nozzytb@gmail.com>
2025-01-12 10:08:51 +0100

    ajout d'un thème personnalisé pour Ant Design

---
+++ src/styles/theme.js
@@ -0,0 +1,11 @@
+const theme = {
+    token: {
+      colorPrimary: '#1677ff',
+    },
+     components: {
+       Button: {
+          colorPrimary: '#1677ff'
+       }
+   }
+ }
+export default theme;
