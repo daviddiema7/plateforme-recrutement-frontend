ad8333883fc2551188c3798143d74c1bb4317874
daviddiema7 <nozzytb@gmail.com>
2025-01-12 01:33:28 +0100

    feat: ajout de recruteursSlice pour gérer l'état des recruteurs dans Redux

--- src/store/slices/recruteurs.js
+++ src/store/slices/recruteurs.js
@@ -4,3 +4,23 @@ const initialState = {
    recruteurs: [],
    selectedRecruteur: null,
 };
+
+
+export const recruteursSlice = createSlice({
+    name: 'recruteurs',
+     initialState,
+      reducers: {
+      addRecruteur: (state, action) => {
+           state.recruteurs.push(action.payload);
+        },
+       setRecruteurs: (state, action) => {
+          state.recruteurs = action.payload
+       },
+        selectRecruteur: (state, action) => {
+            state.selectedRecruteur = action.payload
+        }
+
+      },
+    });
+    export const { addRecruteur, setRecruteurs, selectRecruteur } = recruteursSlice.actions;
+    export default recruteursSlice.reducer;
